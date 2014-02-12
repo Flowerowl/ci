484093a981da5b4ef97dbf6dab782b53e909428c
yuzhe <lazynightz@gmail.com>
2014-02-12 13:59:07 +0800

    update set

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ import sys, os
 setup(name='ci',
       version='${version}',
       description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description="",
+      classifiers=[],
       keywords='',
       author='jackyu',
       author_email='jackyu@sohu-inc.com',
@@ -21,7 +20,5 @@ setup(name='ci',
         'bottle',
         'fabric',
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      entry_points="",
       )
